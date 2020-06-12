d3853f33b26d94c74df1faf46b29d5e1dc3ecec7
Chien Huynh Van <mathenku1998@gmail.com>
2020-06-12 21:53:41 +0700

    first commit

---
+++ Hutech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hutech.Startup))]
+namespace Hutech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
